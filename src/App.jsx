0d09d64c18e5f381db18c5815f0c7d58d8794b14
Edu <kpooh7766@gmail.com>
2023-10-23 20:33:54 +0200

    Feat: Add the second fetch to get image

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import { useEffect, useState } from 'react'
 import React from 'react'
 
+const ENDPOINT_CAT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const ENDPOINT_CAT_RANDOM_IMAGE = `https://cataas.com/cat/says/`
+
 function App() {
-  const [fact, useFact] = useState('gatito')
+  const [fact, setFact] = useState('gatito')
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(ENDPOINT_CAT_RANDOM_FACT)
       .then((resp) => resp.json())
-      .then((data) => console.log(data))
+      //.then((data) => console.log(data.fact))
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+        const firstThreeWord = fact.split(' ', 3).join(' ')
+        //console.log(firstThreeWord)
+
+        fetch(
+          `https://cataas.com/cat/says/${firstThreeWord}?size=50&color=red`,
+        ).then((data) => {
+          //console.log(data.url)
+          const { url } = data
+          setImageUrl(url)
+        })
+      })
   }, [])
+
   return (
     <main>
       <h1>App de gatitos</h1>
-      {fact}
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={`Image with the first three words of: ${fact}`}
+        />
+      )}
     </main>
   )
 }
